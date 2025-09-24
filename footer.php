5b7aca2293a5ef5cf6c409e97691c0aef2ef27fc
franking01 <36132646+franking01@users.noreply.github.com>
2025-09-24 17:42:43 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+</main>
+<footer>
+  <p>&copy; <?php echo date("Y"); ?> PC-Support-Online. All rights reserved.</p>
+  <nav>
+    <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
+  </nav>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
