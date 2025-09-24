e46a4422219174daeaf51fe855c96ec6bbcc9fd1
franking01 <36132646+franking01@users.noreply.github.com>
2025-09-24 17:44:14 -0500

    Create page-remote-support.php

---
+++ page-remote-support.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Remote Support
+*/
+get_header(); ?>
+
+<h2>Remote Support</h2>
+<p>Need help now? Download our remote support tool and we’ll connect securely to your computer.</p>
+<p><a href="#">Download Remote Tool</a> (placeholder link)</p>
+
+<ol>
+  <li>Download and run the remote tool.</li>
+  <li>Share the session ID with our support team.</li>
+  <li>Sit back while we fix your issue remotely.</li>
+</ol>
+
+<?php get_footer(); ?>
